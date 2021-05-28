b1e7885893a2cddd82861341e28674708e4185fd
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-05-28 23:46:35 +0200

    chore: remove unnecessary cast

--- libs/route-config/src/lib/route-tag/not-in-route-tags.pipe.ts
+++ libs/route-config/src/lib/route-tag/not-in-route-tags.pipe.ts
@@ -13,7 +13,7 @@ export class InRouteTags$Pipe<RouteTag extends string = string> implements PipeT
 
   transform(tags: RouteTag[]): Observable<boolean> {
     return this.routeTags$.pipe(
-      map((routeTags) => !!tags.find((tag: RouteTag) => routeTags.includes(tag as never)))
+      map((routeTags) => !!tags.find((tag: RouteTag) => routeTags.includes(tag)))
     );
   }
 }
