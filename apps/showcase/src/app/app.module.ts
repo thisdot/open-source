ae4ef29d79ef3c97bb04bfb81364ef6a3748a0ce
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-06-11 15:41:11 +0200

    feat: material design demoapp initial setup

--- apps/showcase/src/app/app.module.ts
+++ apps/showcase/src/app/app.module.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteConfigModule } from '@this-dot/route-config';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouteConfigModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    RouteConfigModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
