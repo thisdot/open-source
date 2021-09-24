0a0d9da2e987e0ff79137596ea7bd3c2a40036fe
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-09-24 15:54:08 +0200

    fix: tests

--- libs/route-config/src/lib/route-data-has/route-data-has.service.spec.ts
+++ libs/route-config/src/lib/route-data-has/route-data-has.service.spec.ts
@@ -10,7 +10,7 @@ describe('RouteDataHasServiceService', () => {
   //   service = TestBed.inject(RouteDataHasService);
   // });
   //
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    //   expect(service).toBeTruthy();
+  });
 });
