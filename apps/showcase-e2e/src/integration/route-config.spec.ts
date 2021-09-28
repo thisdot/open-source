560ce49d72b533b3963056213ae8aa635bb15252
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-09-28 13:33:22 +0200

    feat: add e2e tests for routeConfigHas directive

--- apps/showcase-e2e/src/integration/route-config.spec.ts
+++ apps/showcase-e2e/src/integration/route-config.spec.ts
@@ -30,6 +30,41 @@ describe(`@thisdot/route-config`, () => {
       .and('contain', "This text is only visible, if there is a 'routeTag.SHOW' in the route data");
   });
 
+  it(`Displays elements, when the appropriate route-tag (show) is set in the custom property in route config`, () => {
+    cy.visit('/route-tags/second');
+    cy.url().should('contain', 'route-tags/second');
+    cy.get('[data-test-id="the route does not have show tag - custom"]').should('not.exist');
+    cy.get('[data-test-id="the route has show tag - custom"]')
+      .should('be.visible')
+      .and(
+        'contain',
+        "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
+      );
+  });
+
+  it(`Displays elements, when custom property is changed and the appropriate route-tag (show) is NOT set in the CHANGED custom property in route config 2`, () => {
+    cy.visit('/route-tags/second');
+    cy.url().should('contain', 'route-tags/second');
+    cy.get('[data-test-id="the route does not have show tag - custom"]').should('not.exist');
+    cy.get('[data-test-id="the route has show tag - custom"]')
+      .should('be.visible')
+      .and(
+        'contain',
+        "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
+      );
+
+    cy.get('[data-test-id="custom prop selector"]')
+      .click()
+      .get('mat-option')
+      .contains('routeTags')
+      .click();
+
+    cy.get('[data-test-id="the route does not have show tag - custom"]')
+      .should('be.visible')
+      .and('contain', "There is no show tag in this route's config property");
+    cy.get('[data-test-id="the route has show tag - custom"]').should('not.exist');
+  });
+
   it(`Does not render elements, when the appropriate route-tag (show) is NOT set in the route config`, () => {
     cy.visit('/route-tags/second');
     cy.url().should('contain', 'route-tags/second');
@@ -37,5 +72,37 @@ describe(`@thisdot/route-config`, () => {
       .should('be.visible')
       .and('contain', "There is no show tag in this route's config");
     cy.get('[data-test-id="the route has show tag"]').should('not.exist');
+  });
+
+  it(`Displays elements, when the appropriate route-tag (show) is NOT set in the custom property in route config`, () => {
+    cy.visit('/route-tags/first');
+    cy.url().should('contain', 'route-tags/first');
+    cy.get('[data-test-id="the route does not have show tag - custom"]')
+      .should('be.visible')
+      .and('contain', "There is no show tag in this route's config property");
+    cy.get('[data-test-id="the route has show tag - custom"]').should('not.exist');
+  });
+
+  it(`Displays different elements, when custom property is changed and the appropriate route-tag (show) is set in the CHANGED custom property in route config`, () => {
+    cy.visit('/route-tags/first');
+    cy.url().should('contain', 'route-tags/first');
+    cy.get('[data-test-id="the route does not have show tag - custom"]')
+      .should('be.visible')
+      .and('contain', "There is no show tag in this route's config property");
+    cy.get('[data-test-id="the route has show tag - custom"]').should('not.exist');
+
+    cy.get('[data-test-id="custom prop selector"]')
+      .click()
+      .get('mat-option')
+      .contains('routeTags')
+      .click();
+
+    cy.get('[data-test-id="the route does not have show tag - custom"]').should('not.exist');
+    cy.get('[data-test-id="the route has show tag - custom"]')
+      .should('be.visible')
+      .and(
+        'contain',
+        "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
+      );
   });
 });
--- libs/route-config/src/lib/route-data-has/route-data-has.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { RouteDataHasDirective } from './route-data-has.directive';
-
-describe('RouteDataHasDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new RouteDataHasDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
