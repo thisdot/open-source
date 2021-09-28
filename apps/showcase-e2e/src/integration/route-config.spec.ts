4c3059533bb6208a4ee876ae31699a767f49cef8
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-09-28 14:33:42 +0200

    feat: add e2e tests for routeConfigHas directive
    rename data test id attributes
    group tests into sections

--- apps/showcase-e2e/src/integration/route-config.spec.ts
+++ apps/showcase-e2e/src/integration/route-config.spec.ts
@@ -1,108 +1,108 @@
 describe(`@thisdot/route-config`, () => {
-  it(`the documentation of the library is always visible`, () => {
-    cy.visit('/route-tags');
-    cy.url().should('contain', 'route-tags/first');
-    cy.get(`[data-test-id="documentation section"]`).should('be.visible');
+  describe('documentation section', () => {
+    it(`the documentation of the library is always visible`, () => {
+      cy.visit('/route-tags');
+      cy.url().should('contain', 'route-tags/first');
+      cy.get(`[data-test-id="documentation section"]`).should('be.visible');
 
-    cy.visit('/route-tags/second');
-    cy.url().should('contain', 'route-tags/second');
-    cy.get(`[data-test-id="documentation section"]`).should('be.visible');
+      cy.visit('/route-tags/second');
+      cy.url().should('contain', 'route-tags/second');
+      cy.get(`[data-test-id="documentation section"]`).should('be.visible');
+    });
   });
 
-  it(`Displays the default title, when the title parameter is not set in the route config object`, () => {
-    cy.visit('/route-tags');
-    cy.url().should('contain', 'route-tags/first');
-    cy.get('h1').should('be.visible').and('contain', 'Default Title');
+  describe('title section', () => {
+    it(`Displays the default title, when the title parameter is not set in the route config object`, () => {
+      cy.visit('/route-tags');
+      cy.url().should('contain', 'route-tags/first');
+      cy.get('h1').should('be.visible').and('contain', 'Default Title');
+    });
+
+    it(`Displays the custom title, when the title parameter is set in the route config object`, () => {
+      cy.visit('/route-tags/second');
+      cy.url().should('contain', 'route-tags/second');
+      cy.get('h1').should('be.visible').and('contain', 'Second Route Title');
+    });
   });
 
-  it(`Displays the custom title, when the title parameter is set in the route config object`, () => {
-    cy.visit('/route-tags/second');
-    cy.url().should('contain', 'route-tags/second');
-    cy.get('h1').should('be.visible').and('contain', 'Second Route Title');
+  describe('tdRouteTag section', () => {
+    it(`Displays elements, when the appropriate route-tag (show) is set in the route config`, () => {
+      cy.visit('/route-tags');
+      cy.url().should('contain', 'route-tags/first');
+      cy.get('[data-test-id="route tag else section"]').should('not.exist');
+      cy.get('[data-test-id="route tag section"]')
+        .should('be.visible')
+        .and(
+          'contain',
+          "This text is only visible, if there is a 'routeTag.SHOW' in the route data"
+        );
+    });
+
+    it(`Does not render elements, when the appropriate route-tag (show) is NOT set in the route config`, () => {
+      cy.visit('/route-tags/second');
+      cy.url().should('contain', 'route-tags/second');
+      cy.get('[data-test-id="route tag else section"]')
+        .should('be.visible')
+        .and('contain', "There is no show tag in this route's config");
+      cy.get('[data-test-id="route tag section"]').should('not.exist');
+    });
   });
 
-  it(`Displays elements, when the appropriate route-tag (show) is set in the route config`, () => {
-    cy.visit('/route-tags');
-    cy.url().should('contain', 'route-tags/first');
-    cy.get('[data-test-id="the route does not have show tag"]').should('not.exist');
-    cy.get('[data-test-id="the route has show tag"]')
-      .should('be.visible')
-      .and('contain', "This text is only visible, if there is a 'routeTag.SHOW' in the route data");
-  });
+  describe('tdRouteDataHas section', () => {
+    it(`Displays custom route tag section, when "show" tag is set in the custom property in route config`, () => {
+      cy.visit('/route-tags/second');
+      cy.url().should('contain', 'route-tags/second');
+      cy.get('[data-test-id="custom route tag else section"]').should('not.exist');
+      cy.get('[data-test-id="custom route tag section"]')
+        .should('be.visible')
+        .and(
+          'contain',
+          "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
+        );
+    });
 
-  it(`Displays elements, when the appropriate route-tag (show) is set in the custom property in route config`, () => {
-    cy.visit('/route-tags/second');
-    cy.url().should('contain', 'route-tags/second');
-    cy.get('[data-test-id="the route does not have show tag - custom"]').should('not.exist');
-    cy.get('[data-test-id="the route has show tag - custom"]')
-      .should('be.visible')
-      .and(
-        'contain',
-        "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
-      );
-  });
+    it(`Displays custom route tag ELSE section, when "show" tag is set in NOT the custom property in route config`, () => {
+      cy.visit('/route-tags/first');
+      cy.url().should('contain', 'route-tags/first');
+      cy.get('[data-test-id="custom route tag else section"]')
+        .should('be.visible')
+        .and('contain', "There is no show tag in this route's config property");
+      cy.get('[data-test-id="custom route tag section"]').should('not.exist');
+    });
 
-  it(`Displays elements, when custom property is changed and the appropriate route-tag (show) is NOT set in the CHANGED custom property in route config 2`, () => {
-    cy.visit('/route-tags/second');
-    cy.url().should('contain', 'route-tags/second');
-    cy.get('[data-test-id="the route does not have show tag - custom"]').should('not.exist');
-    cy.get('[data-test-id="the route has show tag - custom"]')
-      .should('be.visible')
-      .and(
-        'contain',
-        "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
-      );
+    it(`Displays custom route tag ELSE section, when "show" tag is NOT set in the CHANGED custom property in route config`, () => {
+      cy.visit('/route-tags/second');
+      cy.url().should('contain', 'route-tags/second');
 
-    cy.get('[data-test-id="custom prop selector"]')
-      .click()
-      .get('mat-option')
-      .contains('routeTags')
-      .click();
+      cy.get('[data-test-id="custom prop selector"]')
+        .click()
+        .get('mat-option')
+        .contains('routeTags')
+        .click();
 
-    cy.get('[data-test-id="the route does not have show tag - custom"]')
-      .should('be.visible')
-      .and('contain', "There is no show tag in this route's config property");
-    cy.get('[data-test-id="the route has show tag - custom"]').should('not.exist');
-  });
+      cy.get('[data-test-id="custom route tag else section"]')
+        .should('be.visible')
+        .and('contain', "There is no show tag in this route's config property");
+      cy.get('[data-test-id="custom route tag section"]').should('not.exist');
+    });
 
-  it(`Does not render elements, when the appropriate route-tag (show) is NOT set in the route config`, () => {
-    cy.visit('/route-tags/second');
-    cy.url().should('contain', 'route-tags/second');
-    cy.get('[data-test-id="the route does not have show tag"]')
-      .should('be.visible')
-      .and('contain', "There is no show tag in this route's config");
-    cy.get('[data-test-id="the route has show tag"]').should('not.exist');
-  });
+    it(`Displays custom route tag section, when "show" tag is set in the CHANGED custom property in route config`, () => {
+      cy.visit('/route-tags/first');
+      cy.url().should('contain', 'route-tags/first');
 
-  it(`Displays elements, when the appropriate route-tag (show) is NOT set in the custom property in route config`, () => {
-    cy.visit('/route-tags/first');
-    cy.url().should('contain', 'route-tags/first');
-    cy.get('[data-test-id="the route does not have show tag - custom"]')
-      .should('be.visible')
-      .and('contain', "There is no show tag in this route's config property");
-    cy.get('[data-test-id="the route has show tag - custom"]').should('not.exist');
-  });
+      cy.get('[data-test-id="custom prop selector"]')
+        .click()
+        .get('mat-option')
+        .contains('routeTags')
+        .click();
 
-  it(`Displays different elements, when custom property is changed and the appropriate route-tag (show) is set in the CHANGED custom property in route config`, () => {
-    cy.visit('/route-tags/first');
-    cy.url().should('contain', 'route-tags/first');
-    cy.get('[data-test-id="the route does not have show tag - custom"]')
-      .should('be.visible')
-      .and('contain', "There is no show tag in this route's config property");
-    cy.get('[data-test-id="the route has show tag - custom"]').should('not.exist');
-
-    cy.get('[data-test-id="custom prop selector"]')
-      .click()
-      .get('mat-option')
-      .contains('routeTags')
-      .click();
-
-    cy.get('[data-test-id="the route does not have show tag - custom"]').should('not.exist');
-    cy.get('[data-test-id="the route has show tag - custom"]')
-      .should('be.visible')
-      .and(
-        'contain',
-        "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
-      );
+      cy.get('[data-test-id="custom route tag else section"]').should('not.exist');
+      cy.get('[data-test-id="custom route tag section"]')
+        .should('be.visible')
+        .and(
+          'contain',
+          "This text is only visible, if there is a 'routeTag.SHOW' in the route data property"
+        );
+    });
   });
 });
