0894eecb7b89666542d3d7ef6ba2b18243ff6278
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-09-29 12:57:00 +0200

    feat: make utils publishable

--- libs/route-config/src/index.ts
+++ libs/route-config/src/index.ts
@@ -3,4 +3,5 @@ export * from './lib/route-config.service';
 export * from './lib/route-tag/route-tag.directive';
 export * from './lib/route-tag/not-in-route-tags.pipe';
 export * from './lib/route-data';
+export * from './lib/route-data-has/route-data-has.directive';
 export * from './lib/route-data-default-value-token';
