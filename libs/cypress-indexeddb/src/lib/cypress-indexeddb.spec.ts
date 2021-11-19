285cf02f0d310b9958aff4cf8ef5a174af21bf9a
Balázs Tápai <balazs.@thisdot.co>
2021-11-19 14:24:29 +0100

    chore: remove unnecessary spec file

--- libs/cypress-indexeddb/src/lib/cypress-indexeddb.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { cypressIndexeddb } from './cypress-indexeddb';
-//
-// describe('cypressIndexeddb', () => {
-//   it('should work', () => {
-//     expect(cypressIndexeddb()).toEqual('cypress-indexeddb');
-//   });
-// });
