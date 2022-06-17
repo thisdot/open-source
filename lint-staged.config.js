e0a6ecdd7db5f2c290633c71380fac12321c8f94
Balázs Tápai <balazs.@thisdot.co>
2022-06-17 14:48:06 +0200

    chore: put back linting to pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  '*.{html,js,ts}': [
-    'npm run format:write' /* , 'npm run affected:lint:fix' temporarily commented out because it fails in the pre-commit hook */,
-  ],
+  '*.{html,js,ts}': (_) => ['npm run format:write', 'npm run affected:lint:fix'],
   '*.{json,md,css,scss}': ['npm run format:write'],
 };
