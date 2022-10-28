a7ebbfd543dcdfe93362db5b12375539ddc18c55
Temitope Adesoji <temitopeadesoji23@gmail.com>
2022-10-28 12:19:27 +0100

    feat: add ts types to build (#143)
    * feat: add ts types to build
    
    * chore: fix package lock
    
    * chore: fix package.json
    
    * chore: added types to package.json

--- libs/vue-route-guard/vite.config.ts
+++ libs/vue-route-guard/vite.config.ts
@@ -1,23 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
+import dts from 'vite-plugin-dts';
 
 module.exports = defineConfig({
   assetsInclude: /\.(pdf|jpg|png|svg)$/,
   resolve: {
     alias: {
-      '@assets/': `${path.resolve(__dirname, './src/assets')}/`,
       '@lib/': `${path.resolve(__dirname, './src/lib')}/`,
     },
   },
-  plugins: [
-    Vue(),
-    Components({
-      dirs: ['src/lib'],
-    }),
-  ],
+  plugins: [Vue(), dts()],
   build: {
+    emptyOutDir: true,
     lib: {
       entry: path.resolve(__dirname, './src/index.ts'),
       name: 'vue-route-guard',
